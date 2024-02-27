62b12e25f09bd2fdd99061b5ab16dc95d82ea130
301dax <150541297+301dax@users.noreply.github.com>
2024-02-27 13:58:23 +0000

    feat: разработать калькулятор

--- index.js
+++ index.js
@@ -1,62 +1,25 @@
-const shoppingCart = {
-    items: [], // Массив товаров
-    total: 0,  // Общая стоимость товаров
-  
-    // Метод добавления товара в корзину
-    addItem: function(name, price, quantity) {
-      const item = {
-        name: name,
-        price: price,
-        quantity: quantity
-      };
+const readlineSync = require('readline-sync');
 
-      this.items.push(item);
+const calculator = {
+  a: 0,
+  b: 0,
 
-      this.calculateTotal(); // Обновляем общую стоимость
-    },
-  
-    // Метод удаления товара из корзины по имени
-    removeItem: function(name) {
-      this.items = this.items.filter(item => item.name !== name);
+  read() {
+    this.a = parseFloat(readlineSync.question('Первое значение: '));
+    this.b = parseFloat(readlineSync.question('Второе значение: '));
+  },
 
-      this.calculateTotal(); // Обновляем общую стоимость
-    },
-  
-    // Метод обновления количества товара
-    updateQuantity: function(name, quantity) {
-      const item = this.items.find(item => item.name === name);
+  sum() {
+    return this.a + this.b;
+  },
 
-      if (item) {
-        item.quantity = quantity;
+  mul() {
+    return this.a * this.b;
+  },
+};
 
-        this.calculateTotal(); // Обновляем общую стоимость
-      }
-    },
-  
-    // Метод вычисления общей стоимости товаров в корзине
-    calculateTotal: function() {
-      this.total = this.items.reduce((totalCost, item) => totalCost + item.price * item.quantity, 0);
-    },
-  
-    // Метод очистки корзины
-    clearCart: function() {
-      this.items = [];
-      this.total = 0;
-    },
-  
-    // Метод применения скидки
-    applyDiscount: function(discountCode) {
-      const discountRates = {
-        'DISCOUNT10': 0.1, // 10% скидка
-        'DISCOUNT20': 0.2  // 20% скидка
-      };
-      
-      const discount = discountRates[discountCode];
+calculator.read();
+console.log(`Сумма: ${calculator.sum()}`);
+console.log(`Произведение: ${calculator.mul()}`);
 
-      if (discount) {
-        this.total = this.total * (1 - discount);
-      }
-    }
-  };
-
-module.exports = shoppingCart
+module.exports = calculator;
